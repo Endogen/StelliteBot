8ddf5598e484725bed42dd07c873b63e8664caa2
Endogen <081081@gmx.net>
2018-08-07 01:44:47 +0200

    - Added wiki info-graphics - Added some requirements to run the bot - Added hardcoded dict with all wiki search terms - Added decorator for restricted access - Added update, restart and shutdown commands

--- stellite_bot.py
+++ stellite_bot.py
@@ -1,6 +1,10 @@
 import json
 import logging
 import os
+import requests
+import sys
+import time
+import threading
 
 import TradeOgre
 
@@ -8,7 +12,23 @@ from telegram.ext import Updater, CommandHandler, MessageHandler
 from telegram.ext.filters import Filters
 from telegram import ParseMode
 
-# Check if file 'config.json' exists. Exit if not.
+# Key name for temporary user in config
+TMP_RSTR_USR = "restart_user"
+WIKI_DICT = {
+    "cryptonote": "CryptoNote.png",
+    "double-spend": "double-spending proof.png",
+    "egalitarian": "egalitarian PoW.png",
+    "pow": "egalitarian PoW.png",
+    "consensus": "egalitarian PoW.png",
+    "exchange": "exchanges.png",
+    "ipfs": "IPFS ZeroNet.png",
+    "zeronet": "IPFS ZeroNet.png",
+    "mobile": "mobile miner.jpg",
+    "transaction": "unlinkable transactions.png"
+}
+
+
+# Read configuration file
 if os.path.isfile("config.json"):
     # Read configuration
     with open("config.json") as config_file:
@@ -29,64 +49,164 @@ job_queue = updater.job_queue
 trade_ogre = TradeOgre.API()
 
 
+# Decorator to restrict access if user is not an admin
+def restrict_access(func):
+    def _restrict_access(bot, update):
+        user_id = update.message.from_user.id
+        admin_list = bot.get_chat_administrators(update.message.chat_id)
+
+        access = False
+
+        for admin in admin_list:
+            if user_id == admin.user.id:
+                access = True
+
+        if access:
+            return func(bot, update)
+        else:
+            bot.send_message(update.message.chat_id, text="Access denied")
+            return
+
+    return _restrict_access
+
+
 # Get current price of XTL for a given asset pair
 def price(bot, update):
-	xtl_ticker = trade_ogre.ticker(config["pairing_asset"] + "-XTL")
-	if xtl_ticker["success"]:
-		price = xtl_ticker["price"]
+    xtl_ticker = trade_ogre.ticker(config["pairing_asset"] + "-XTL")
+    xtl_price = xtl_ticker["price"]
 
-		msg = "`" + "XTL on TradeOgre: " + price + " " + config["pairing_asset"] + "`"
-		bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
+    if xtl_ticker["success"]:
+        msg = "`" + "XTL on TradeOgre: " + xtl_price + " " + config["pairing_asset"] + "`"
+        bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
+    else:
+        msg = "`Couldn't retrieve current XTL price`"
+        bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
 def auto_reply(bot, update):
-	text = update.message.text
+    text = update.message.text
 
-	if "when moon".lower() in text.lower():
-		video = open('soon_moon.mp4', 'rb')
-		update.message.reply_video(video, parse_mode=ParseMode.MARKDOWN)
-
-
-# TODO: Just for testing purposes
-def fake_db(search):
-	db = {"stellite": "Stellite solves issues that have been puzzling cryptocurrency developers for years on topics such as an efficient and decentralized method of distributing peer list without hard coding them, using the billions of small devices to combine their computing power and form a part of a huge Proof-of-work network which helps people use their own small devices to be not only a supporting factor to the network but also be provisioned a small reward in doing so. Stellite is unique in linking both the IPFS and ZeroNet technologies into a cryptocurrency and scaling it globally for both mobile and desktop usage.",
-	"stellitepay": "Imagine having a platform like PayPal for crypto. Send and receive payments easily, easy integration for merchants."}
-
-	if search.lower() in db:
-		return db[search.lower()]
-	else:
-		return None
+    if "when moon".lower() in text.lower():
+        video = open(os.path.join(config["res_folder"], "soon_moon.mp4"), 'rb')
+        update.message.reply_video(video, parse_mode=ParseMode.MARKDOWN)
 
 
+# TODO: Add a list with all search terms
 def wiki(bot, update, args):
-	if len(args) > 0 and args[0]:
-		result = fake_db(args[0])
-		if result:
-			msg = "`" + result + "`"
-		else:
-			msg = "`No entry found`"
-	else:
-		msg = "`No search argument provided`"
+    if len(args) > 0 and args[0]:
+        if args[0].lower() in WIKI_DICT:
+            path = WIKI_DICT[args[0].lower()]
 
-	bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
+        if path:  # TODO: What if 'path' variable doesn't exist?
+            image = open(os.path.join(config["res_folder"], path), 'rb')
+            update.message.reply_photo(image, parse_mode=ParseMode.MARKDOWN)
+            return
+        else:
+            msg = "`No entry found`"
+    else:
+        msg = "`No search argument provided`"
+        # TODO: Here we should display the list with all wiki entries
+
+    bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
-# TODO: Use for 'restart', ...
-def is_admin(bot, update):
-	# Telegram.ChatMember.status. used by bot.get_chat_member(chat_id, user_id). And then getting status in it.
-	pass
+@restrict_access
+def update_bot(bot, update):
+    msg = "Bot is updating..."
+    update.message.reply_text(msg)
+
+    # Get newest version of this script from GitHub
+    headers = {"If-None-Match": config["update_hash"]}
+    github_script = requests.get(config["update_url"], headers=headers)
+
+    # Status code 304 = Not Modified
+    if github_script.status_code == 304:
+        msg = "You are running the latest version"
+        update.message.reply_text(msg)
+    # Status code 200 = OK
+    elif github_script.status_code == 200:
+        # Get github 'config.json' file
+        last_slash_index = config["update_url"].rfind("/")
+        github_config_path = config["update_url"][:last_slash_index + 1] + "config.json"
+        github_config_file = requests.get(github_config_path)
+        github_config = json.loads(github_config_file.text)
+
+        # Compare current config keys with
+        # config keys from github-config
+        if set(config) != set(github_config):
+            # Go through all keys in github-config and
+            # if they are not present in current config, add them
+            for key, value in github_config.items():
+                if key not in config:
+                    config[key] = value
+
+        # Save current ETag (hash) of bot script in github-config
+        e_tag = github_script.headers.get("ETag")
+        config["update_hash"] = e_tag
+
+        # Save changed github-config as new config
+        with open("config.json", "w") as cfg:
+            json.dump(config, cfg, indent=4)
+
+        # Get the name of the currently running script
+        path_split = os.path.split(str(sys.argv[0]))
+        filename = path_split[len(path_split)-1]
+
+        # Save the content of the remote file
+        with open(filename, "w") as file:
+            file.write(github_script.text)
+
+        msg = "Update finished..."
+        update.message.reply_text(msg)
+
+        # Restart the bot
+        restart_bot(bot, update)
 
 
+@restrict_access
+def restart_bot(bot, update):
+    msg = "Restarting bot..."
+    update.message.reply_text(msg)
+
+    # Set temporary restart-user in config
+    config[TMP_RSTR_USR] = update.message.chat_id
+
+    # Save changed config
+    with open("config.json", "w") as cfg:
+        json.dump(config, cfg, indent=4)
+
+    # Restart bot
+    time.sleep(0.2)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+
+# This needs to be run on a new thread because calling 'updater.stop()' inside a
+# handler (shutdown_cmd) causes a deadlock because it waits for itself to finish
+def shutdown():
+    updater.stop()
+    updater.is_idle = False
+
+
+# Terminate this script
+@restrict_access
+def shutdown_bot(bot, update):
+    update.message.reply_text("Shutting down...")
+
+    # See comments on the 'shutdown' function
+    threading.Thread(target=shutdown).start()
+
+
+@restrict_access
 def ban(bot, update):
-	# http://python-telegram-bot.readthedocs.io/en/stable/telegram.chat.html#telegram.Chat.get_member
-	# kick_member(*args, **kwargs)
-	pass
+    # http://python-telegram-bot.readthedocs.io/en/stable/telegram.chat.html#telegram.Chat.get_member
+    # kick_member(*args, **kwargs)
+    pass
 
 
 # Handle all telegram and telegram.ext related errors
 def handle_telegram_error(bot, update, error):
-	error_str = "Update '%s' caused error '%s'" % (update, error)
-	logger.log(logging.DEBUG, error_str)
+    error_str = "Update '%s' caused error '%s'" % (update, error)
+    logger.log(logging.DEBUG, error_str)
 
 
 # Log all errors
@@ -94,9 +214,24 @@ dispatcher.add_error_handler(handle_telegram_error)
 
 # Add command handlers to dispatcher
 dispatcher.add_handler(CommandHandler("price", price))
+dispatcher.add_handler(CommandHandler("update", update_bot))
+dispatcher.add_handler(CommandHandler("restart", restart_bot))
+dispatcher.add_handler(CommandHandler("shutdown", shutdown_bot))
 dispatcher.add_handler(CommandHandler("wiki", wiki, pass_args=True))
 dispatcher.add_handler(MessageHandler(Filters.text, auto_reply))
 
 
 updater.start_polling(clean=True)
 updater.idle()
+
+# Send message that bot is started after restart
+if TMP_RSTR_USR in config:
+    msg = "Bot started..."
+    updater.bot.send_message(chat_id=config[TMP_RSTR_USR], text=msg)
+
+    # Remove temporary key from config
+    config.pop(TMP_RSTR_USR, None)
+
+    # Save changed config
+    with open("config.json", "w") as cfg:
+        json.dump(config, cfg, indent=4)
